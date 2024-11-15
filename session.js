9637e588fc2eb84e8d7c1ad60b2e67716df1f145
botmate2011 <sethikadamsuwaebay@gmail.com>
2024-11-15 15:31:03 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=WNtRHKBI#mr-s4fkVUEzXHX8dSoB7iAuycV7l1wDik9VNpxyjA2I",
 };
